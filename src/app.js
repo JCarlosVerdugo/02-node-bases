8107c88abf7bf12912fc8e90a516253e2ed04dac
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-07 08:07:23 -0700

    working with callback

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-require('./js-foundation/02-destructuring')
+const  { getUserById } = require('./js-foundation/03-callbacks')
+
+const id = 1;
+
+getUserById(id, function(error, user) {
+    if ( error ) {
+        throw new Error(error)
+    }
+
+    console.log(user);
+})
